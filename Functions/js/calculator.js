94511be616e49a99940ac6ef770016f6bb5c9da2
pengdev-works <elizaliecorpuz@gmail.com>
2026-01-28 11:20:13 +0800

    calculator

--- Functions/js/calculator.js
+++ Functions/js/calculator.js
@@ -1,13 +1,48 @@
-let display = document.getElementById("display");
+const display = document.getElementById("display");
+const display1 = document.getElementById("display1");
 
-    function press(value) {
-        display.value += value;
-    }
+let firstValue = "";
+let operator = "";
 
-    function allClear() {
-        display.value = "";
-    }
+function appendValue(value) {
+  if (display.value === "0") {
+    display.value = value;
+  } else {
+    display.value += value;
+  }
+}
+function ope(value) {
+  firstValue = display.value;
+  operator = value;
+  display1.value = operator;
+  display.value = "0";
+}
 
-    function clearLast() {
-        display.value = display.value.slice(0, -1);
-    }
+function calculate() {
+  const secondValue = display.value;
+  let result;
+
+  if (operator === "+") {
+    result = Number(firstValue) + Number(secondValue);
+  } else if (operator === "-") {
+    result = Number(firstValue) - Number(secondValue);
+  } else if (operator === "*") {
+    result = Number(firstValue) * Number(secondValue);
+  } else if (operator === "/") {
+    result = Number(firstValue) / Number(secondValue);
+  }
+
+  display.value = result;
+  display1.value = "";
+}
+
+
+function allClear() {
+  display.value = "0";
+  display1.value = "";
+}
+
+function clearLast() {
+  display.value = display.value.slice(0, -1);
+  if (display.value === "") display.value = "0";
+}
